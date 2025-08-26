e01424bec38519875148ec1cc27c189847560e38
Javed Mohamed <jav@deadbeef.moe>
2025-08-25 23:01:19 -0500

    Add bare darwin config

---
+++ configuration.nix
@@ -0,0 +1,5 @@
+{
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  nix.enable = false;
+  system.stateVersion = 6;
+}
--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2505";
+    nix-darwin.url = "https://flakehub.com/f/nix-darwin/nix-darwin/0.2505";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nix-darwin }:
     let
       forAllSystems = f:
         nixpkgs.lib.genAttrs [
@@ -15,6 +17,10 @@
       devShells = forAllSystems (pkgs: {
         default = pkgs.callPackage ./shell.nix {};
       });
+
+      darwinConfigurations."Javeds-MacBook-Air" = nix-darwin.lib.darwinSystem {
+        modules = [ ./configuration.nix ];
+      };
     }
   ;
 }
